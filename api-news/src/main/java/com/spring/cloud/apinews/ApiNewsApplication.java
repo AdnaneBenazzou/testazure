dec142988eafa383f60ea6cab3cb1c8ac281d07d
mderemer <external.z05mdere@decathlon.com>
2018-05-28 14:09:05 +0200

    feat(part1): add data, update dependencies feat(part1): add data, update dependencies

--- api-news/src/main/java/com/spring/cloud/apinews/ApiNewsApplication.java
+++ api-news/src/main/java/com/spring/cloud/apinews/ApiNewsApplication.java
@@ -1,6 +1,9 @@
 package com.spring.cloud.apinews;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.spring.cloud.apinews.pojo.News;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -12,16 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ApiNewsApplication {
 
-	@Value("${spring.application.name}")
-	private String appName;
+	private static Map<Integer,News> newsMap = new HashMap<>();
 
 	public static void main(String[] args) {
+		newsMap.put(1, new News(1, "Fallout 4", "Sortie de Fallout 4 en GOTY !",1, 1));
+		newsMap.put(2, new News(2,"Java", "Sortie de Java 10",1, 2));
+		newsMap.put(3, new News(3,"Soluce", "Soluce MassEffect",1, 1));
 		SpringApplication.run(ApiNewsApplication.class, args);
 	}
 
 	@RequestMapping("/news")
-	public String getAllNews() {
-		return "Fallout 4 is back !";
+	public Collection<News> getAllNews() {
+		return newsMap.values();
 	}
 
 }
